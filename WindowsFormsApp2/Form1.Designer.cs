f929997bbeb4bd65f18a075f6b9d8c429ea954bc
jnabin-dev <nabin.skytech@gmail.com>
2025-01-20 18:30:20 +0600

    extract more data

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClosedXML.Excel;
 using OpenQA.Selenium;
@@ -21,6 +24,8 @@ namespace WindowsFormsApp2
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Button btnUpload;
         private System.Windows.Forms.Button btnStart;
+        //private System.Windows.Forms.DataGridView dataGridView = new System.Windows.Forms.DataGridView();
+        //private DataTable resultsDataTable = new DataTable();
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -50,7 +55,17 @@ namespace WindowsFormsApp2
             this.lblStatus = new System.Windows.Forms.Label();
             this.btnUpload = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
+            //((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+
+            //this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            //this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            //this.dataGridView.Name = "dataGridView";
+            //this.dataGridView.RowTemplate.Height = 24;
+            //this.dataGridView.Size = new System.Drawing.Size(760, 400);
+            //this.dataGridView.TabIndex = 0;
+
+
             this.lblStatus.AutoSize = true;
             this.lblStatus.Location = new System.Drawing.Point(12, 50); // Position it appropriately
             this.lblStatus.Name = "lblStatus";
@@ -79,17 +94,43 @@ namespace WindowsFormsApp2
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 200); // Adjust size as needed
+            //this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.btnStart);
             this.Name = "MainForm";
             this.Text = "Google Maps Crawler";
+            //((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        //private void InitializeDataTable()
+        //{
+        //    // Define the DataTable with columns
+        //    resultsDataTable = new DataTable();
+        //    resultsDataTable.Columns.Add("Keyword");
+        //    resultsDataTable.Columns.Add("Result Title");
+        //    resultsDataTable.Columns.Add("Category");
+        //    resultsDataTable.Columns.Add("Location");
+        //    resultsDataTable.Columns.Add("Contact Number");
+        //    resultsDataTable.Columns.Add("Email");
+        //    resultsDataTable.Columns.Add("Website");
+        //    resultsDataTable.Columns.Add("Facebook");
+        //    resultsDataTable.Columns.Add("Linkedin");
+        //    resultsDataTable.Columns.Add("Twitter");
+        //    resultsDataTable.Columns.Add("Youtube");
+        //    resultsDataTable.Columns.Add("Instagram");
+        //    resultsDataTable.Columns.Add("Pinterest");
+        //    resultsDataTable.Columns.Add("Ratiung");
+        //    resultsDataTable.Columns.Add("Review Count");
+
+        //    // Bind the DataTable to the DataGridView
+        //    dataGridView.DataSource = resultsDataTable;
+        //}
 
         private void btnUpload_Click(object sender, EventArgs e)
         {
@@ -120,20 +161,20 @@ namespace WindowsFormsApp2
             }
 
             lblStatus.Text = "Crawling started...";
-            new Thread(() => StartCrawling()).Start();
+            //InitializeDataTable();
+            new Thread(async () => await StartCrawlingAsync()).Start();
         }
 
 
-        private void StartCrawling()
+        private async Task StartCrawlingAsync()
         {
             try
             {
-                var results = new System.Collections.Generic.List<(string SearchTerm, string ResultTitle, string ReviewCount, string Rating, string ContactNumber, string Category, string Address)>();
+                var results = new System.Collections.Generic.List<(string SearchTerm, string ResultTitle, string ReviewCount, string Rating, string ContactNumber, string Category, string Address, Dictionary<string, string>, string companyWebsite)>();
 
                 new DriverManager().SetUpDriver(new ChromeConfig()); // Automatically downloads ChromeDriver
                 ChromeOptions options = new ChromeOptions();
                 IWebDriver driver = new ChromeDriver(options);
-
                 //ChromeOptions options = new ChromeOptions();
                 //string driverPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources");
                 //IWebDriver driver = new ChromeDriver(driverPath);
@@ -163,6 +204,8 @@ namespace WindowsFormsApp2
                         // Extract search results
                         var resultElements = driver.FindElements(By.XPath("//div[@class='bfdHYd Ppzolf OFBs3e  ']"));
                         int i = 0;
+                        ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                        service.HideCommandPromptWindow = true;
                         foreach (var resultElement in resultElements)
                         {
                             try
@@ -175,7 +218,16 @@ namespace WindowsFormsApp2
                                     i++;
                                     continue; // Skip already processed results
                                 }
-                                    
+                                //lcr4fd S9kvJb
+                                Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+                                var linkElement = resultElement.FindElements(By.ClassName("lcr4fd"));
+                                string hrefValue = string.Empty;
+                                if (linkElement.Count > 0)
+                                {
+                                    hrefValue = linkElement[0].GetAttribute("href");
+                                    var task = Task.Run(() => FetchSocialMediaLinks(hrefValue, service));
+                                    keyValuePairs = await task;
+                                }
                                 processedResults.Add(title);
                                 var clickableElement = driver.FindElements(By.CssSelector("a.hfpxzc"));
                                // ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", clickableElement[i]);
@@ -216,8 +268,13 @@ namespace WindowsFormsApp2
                                 {
                                 }
 
-                                results.Add((term, title, reviewCount, rating, contactNumber, category, location));
-                                //driver.Navigate().Back();
+                                results.Add((term, title, reviewCount, rating, contactNumber, category, location, keyValuePairs, hrefValue));
+                                //Invoke(new Action(() =>
+                                //{
+                                //    // Add a new row with the extracted data
+                                //    resultsDataTable.Rows.Add(term, title, reviewCount, rating, contactNumber, category, location, "", "", "", "", "", "", hrefValue);
+                                //}));
+                                driver.Navigate().Back();
                                 Thread.Sleep(2000);
                             }
                             catch (Exception ex)
@@ -275,6 +332,19 @@ namespace WindowsFormsApp2
             }
         }
 
+    //    private string GetRandomUserAgent()
+    //    {
+    //        var userAgents = new List<string>
+    //{
+    //    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    //    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    //    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0"
+    //};
+
+    //        Random rand = new Random();
+    //        return userAgents[rand.Next(userAgents.Count)];
+    //    }
+
         private List<string> GetRatingReview(string input)
         {
             List<string>  ratings = new List<string>();
@@ -302,6 +372,152 @@ namespace WindowsFormsApp2
 
             return ratings;
         }
+
+        public Dictionary<string, string> FetchSocialMediaLinks(string businessUrl, ChromeDriverService service)
+        {
+            var socialLinks = new Dictionary<string, string>();
+            if (businessUrl == null || businessUrl.Length == 0) {  return socialLinks; }
+            // Launch a new browser (this could be in a separate ChromeDriver instance)
+            var options = new ChromeOptions();
+            options.AddArguments("headless", "disable-gpu", "no-sandbox");
+            options.AddArguments("--blink-settings=imagesEnabled=false");  // Disable images
+            options.AddArguments("--disable-css");  // Disable CSS
+            options.AddArguments("--disable-javascript");  // Disable JavaScript
+            
+
+            var driver = new ChromeDriver(service, options);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            try
+            {
+                driver.Navigate().GoToUrl(businessUrl);
+
+                // Fetch social media links from the business website (e.g., from footer or social media icons)
+                var socialMediaSelectors = new List<string>
+                {
+                    "a[href*='facebook.com']",
+                    "a[href*='x.com']",
+                    "a[href*='linkedin.com']",
+                    "a[href*='instagram.com']",
+                    "a[href*='youtube.com']",
+                    "a[href*='pinterest.com']",
+                };
+
+                foreach (var selector in socialMediaSelectors)
+                {
+                    var elements = driver.FindElements(By.CssSelector(selector));
+                    foreach (var element in elements)
+                    {
+                        var link = element.GetAttribute("href");
+                        var links = driver.FindElements(By.TagName("a"));
+                        if (!string.IsNullOrEmpty(link) && (link.Contains("http://") || link.Contains("https://")))
+                        {
+                            bool isInvalidValid = 
+                                link == "https://www.linkedin.com/" || 
+                                link == "https://www.facebook.com/" || 
+                                link == "https://www.twitter.com/" || 
+                                link == "https://instagram.com/" || 
+                                link == "https://youtube.com/" || 
+                                link == "https://pinterest.com/";
+                            Uri uri = new Uri(link);
+                            string host = uri.Host.Replace("www.", ""); // Remove "www."
+                            string baseDomain = host.Split('.')[0];
+                            if (isInvalidValid) link = string.Empty;
+                            if(!isInvalidValid)
+                            {
+                                if (!socialLinks.ContainsKey(baseDomain))
+                                {
+                                    socialLinks.Add(baseDomain, link);
+                                }
+                            }
+                                
+                        }
+                    }
+                }
+
+                var emails = FetchEmails(driver);
+
+                // Combine all emails into one string (comma-separated)
+                if (emails.Count > 0)
+                {
+                    string combinedEmails = string.Join(", ", emails);
+                    socialLinks["emails"] = combinedEmails; // Single key for all emails
+                }
+                else
+                {
+                    socialLinks["emails"] = "No emails found";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching social media links for {businessUrl}: {ex.Message}");
+            }
+            finally
+            {
+                driver.Quit();
+            }
+
+            return socialLinks;
+        }
+
+        public static List<string> FetchEmails(IWebDriver driver)
+        {
+            List<string> emails = new List<string>();
+            var links = driver.FindElements(By.TagName("a"));
+            string emailPattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+            var elements = driver.FindElements(By.XPath("//*[text()]"));
+            foreach (var element in elements)
+            {
+                try
+                {
+                    string text = element.Text;
+
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        // Match all email-like patterns in the text
+                        var matches = Regex.Matches(text, emailPattern);
+                        foreach (Match match in matches)
+                        {
+                            string email = match.Value;
+
+                            // Avoid duplicates
+                            if (!emails.Contains(email))
+                            {
+                                emails.Add(email);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing element: {ex.Message}");
+                    return emails;
+                }
+            }
+            //foreach (var link in links)
+            //{
+            //    try
+            //    {
+            //        string href = link.GetAttribute("href");
+
+            //        if (!string.IsNullOrEmpty(href) && href.StartsWith("mailto:"))
+            //        {
+            //            string email = href.Replace("mailto:", "").Trim();
+            //            if (!emails.Contains(email) && Regex.IsMatch(email, emailPattern))
+            //            {
+            //                emails.Add(email);
+            //            }
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine($"Error processing link: {ex.Message}");
+            //    }
+            //}
+
+            return emails;
+        }
+
+
 
         private bool ScrollToLoadMoreResults(IWebDriver driver, IWebElement mapContainer)
         {
@@ -343,7 +559,7 @@ namespace WindowsFormsApp2
             //}
         }
 
-        private void ExportToExcel(System.Collections.Generic.List<(string SearchTerm, string ResultTitle, string ReviewCount, string Rating, string ContactNumber, string Category, string Address)> results)
+        private void ExportToExcel(System.Collections.Generic.List<(string SearchTerm, string ResultTitle, string ReviewCount, string Rating, string ContactNumber, string Category, string Address, Dictionary<string, string> socialMedias, string hrefValue)> results)
         {
             try
             {
@@ -355,8 +571,16 @@ namespace WindowsFormsApp2
                     worksheet.Cell(1, 3).Value = "Category";
                     worksheet.Cell(1, 4).Value = "Location";
                     worksheet.Cell(1, 5).Value = "Contact Number";
-                    worksheet.Cell(1, 6).Value = "Rating";
-                    worksheet.Cell(1, 7).Value = "Review Count";
+                    worksheet.Cell(1, 6).Value = "Email";
+                    worksheet.Cell(1, 7).Value = "Website";
+                    worksheet.Cell(1, 8).Value = "Facebook";
+                    worksheet.Cell(1, 9).Value = "LinkedIn";
+                    worksheet.Cell(1, 10).Value = "Twitter";
+                    worksheet.Cell(1, 11).Value = "Youtube";
+                    worksheet.Cell(1, 12).Value = "Instagram";
+                    worksheet.Cell(1, 13).Value = "Pinterest";
+                    worksheet.Cell(1, 14).Value = "Rating";
+                    worksheet.Cell(1, 15).Value = "Review Count";
 
                     for (int i = 0; i < results.Count; i++)
                     {
@@ -365,8 +589,16 @@ namespace WindowsFormsApp2
                         worksheet.Cell(i + 2, 3).Value = results[i].Category;
                         worksheet.Cell(i + 2, 4).Value = results[i].Address;
                         worksheet.Cell(i + 2, 5).Value = results[i].ContactNumber;
-                        worksheet.Cell(i + 2, 6).Value = results[i].ReviewCount;
-                        worksheet.Cell(i + 2, 7).Value = results[i].Rating;
+                        worksheet.Cell(i + 2, 6).Value = results[i].socialMedias.ContainsKey("emails") ? results[i].socialMedias["emails"] : string.Empty;
+                        worksheet.Cell(i + 2, 7).Value = results[i].hrefValue;
+                        worksheet.Cell(i + 2, 8).Value = results[i].socialMedias.ContainsKey("facebook") ? results[i].socialMedias["facebook"] : string.Empty;
+                        worksheet.Cell(i + 2, 9).Value = results[i].socialMedias.ContainsKey("linkedin") ? results[i].socialMedias["linkedin"] : string.Empty;
+                        worksheet.Cell(i + 2, 10).Value = results[i].socialMedias.ContainsKey("x") ? results[i].socialMedias["x"] : string.Empty;
+                        worksheet.Cell(i + 2, 11).Value = results[i].socialMedias.ContainsKey("youtube") ? results[i].socialMedias["youtube"] : string.Empty;
+                        worksheet.Cell(i + 2, 12).Value = results[i].socialMedias.ContainsKey("instagram") ? results[i].socialMedias["instagram"] : string.Empty;
+                        worksheet.Cell(i + 2, 13).Value = results[i].socialMedias.ContainsKey("pinterest") ? results[i].socialMedias["pinterest"] : string.Empty;
+                        worksheet.Cell(i + 2, 14).Value = results[i].ReviewCount;
+                        worksheet.Cell(i + 2, 15).Value = results[i].Rating;
                     }
 
                     string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "GoogleMapsResults.xlsx");
